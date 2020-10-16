7f196311aefd914209b724d91b56bbed4337b237
Wisdom Praise <wisdompraise968@gmail.com>
2020-10-16 12:44:10 +0100

    Conditionally rendered some section of views

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -32,8 +32,12 @@ class ProjectsController < ApplicationController
     end
 
     def show
-        @colaborator = Colaborator.new
-        @user_id = current_user.id
+      @colaborator = Colaborator.new
+      @user_id = current_user.id
+        if params[:assoc]
+           @assoc = true
+           @project = Project.find(params[:id])
+        end
         @task = @project.tasks.build
         @collaborators = Colaborator.where(:project_id => @project.id)
     end
