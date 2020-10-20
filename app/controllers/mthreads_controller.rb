9b09ead3cbf3b45b2aca13eea53dfca23b8a84c8
Ibrahim Bashir Nayaya <ibrahimnayaya25@gmail.com>
2020-10-20 11:55:22 -0700

    additional change to threads

---
+++ app/controllers/mthreads_controller.rb
@@ -0,0 +1,53 @@
+class MthreadsController < ApplicationController
+    before_action :set_project
+    before_action :set_mthread, only: [:show, :edit, :update, :destroy]
+
+    def index
+        
+    end
+
+    def new
+        
+    end
+
+    def create
+        @mthread = @project.mthreads.build(mthread_params)
+        
+        if @mthread.save
+          redirect_to(request.referrer, success: "Added thread")
+        else
+            p @mthread.errors
+            redirect_to(request.referrer, danger: "Unable to add thread")
+        end
+    end
+
+    def show
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+    def destroy
+        
+    end
+
+    private
+    def set_project
+        @project = Project.find(params[:project_id])
+    end
+
+    def set_mthread
+        @mthread = @project.mthreads.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def mthread_params
+    params.permit(:title, :user_id, :project_id)
+    end
+end
